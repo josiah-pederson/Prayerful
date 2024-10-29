cef962be9060e79becb59a96efb5dcdb018d8ab3
jinspins <uncleben096@gmail.com>
2024-10-28 19:03:19 -0500

    Initial Commit

---
+++ Prayerful/PrayerfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerfulApp.swift
+//  Prayerful
+//
+//  Created by Josiah Pederson on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PrayerfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
