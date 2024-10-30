8e5268ca7ff6f72e1a4f4e4c5fa0f01bb50ad77c
jinspins <uncleben096@gmail.com>
2024-10-29 21:14:15 -0500

    Reorganized filesystem

--- Prayerful/Models/Audio/AudioRecordingStatus.swift
+++ Prayerful/Models/Audio/AudioRecordingStatus.swift
@@ -8,15 +8,25 @@
 import Foundation
 
 /// Tracks the status of an audio recorder
-enum RecordingStatus: Equatable {
+enum AudioRecordingStatus {
 	case stopped
 	case recording
 	case paused
 	case preparing
 	case finalizing
 	case error(Error)
+}
 
-	static func == (lhs: RecordingStatus, rhs: RecordingStatus) -> Bool {
+extension AudioRecordingStatus: Equatable {
+	
+	// MARK: - Equatable conformance
+	
+	/// Compares two instances of Self to determine if they are equal
+	/// - Parameters:
+	///   - lhs: The first (left hand side) AudioRecordingStatus to compare
+	///   - rhs: The second (right hand side) AudioRecordingStatus to compare
+	/// - Returns: A boolean indicating if they are the same or not
+	static func == (lhs: Self, rhs: Self) -> Bool {
 		switch (lhs, rhs) {
 		case (.recording, .recording), (.stopped, .stopped), (.paused, .paused), (.preparing, .preparing), (.finalizing, .finalizing):
 			return true
