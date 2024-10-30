ff7685b4a722728b9613cc916fee9bb0bdf6ee31
jinspins <uncleben096@gmail.com>
2024-10-29 21:34:45 -0500

    Created persistent storage models for prayers and threads

---
+++ Prayerful/Models/Persistent/PrayerRecording.swift
@@ -0,0 +1,26 @@
+//
+//  PrayerRecording.swift
+//  Prayerful
+//
+//  Created by Josiah Pederson on 10/29/24.
+//
+
+import Foundation
+import SwiftData
+
+/// A recording of a prayer as part of a ``PrayerThread``
+///
+/// > Note: This is a SwiftData persistent storage model
+@Model
+class PrayerRecording {
+	var filePath: URL
+	var duration: Double
+	var timestamp: Date
+	var prayerThread: PrayerThread?
+
+	init(filePath: URL, duration: Double, timestamp: Date = Date()) {
+		self.filePath = filePath
+		self.duration = duration
+		self.timestamp = timestamp
+	}
+}
---
+++ Prayerful/Models/Persistent/PrayerThread.swift
@@ -0,0 +1,28 @@
+//
+//  PrayerThread.swift
+//  Prayerful
+//
+//  Created by Josiah Pederson on 10/29/24.
+//
+
+import Foundation
+import SwiftData
+
+/// A thread of prayer recordings about a topic
+///
+/// Uses the ``PrayerRecording`` model for saving recordings
+///
+/// >Note: This is a SwiftData persistent storage model
+@Model
+class PrayerThread {
+	var title: String
+	var creationDate: Date
+	@Relationship(deleteRule: .cascade, inverse: \PrayerRecording.prayerThread)
+	var recordings: [PrayerRecording]
+
+	init(title: String, creationDate: Date = Date()) {
+		self.title = title
+		self.creationDate = creationDate
+		self.recordings = []
+	}
+}
